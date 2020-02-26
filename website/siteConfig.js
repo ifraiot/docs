07fc5c1de953ab30babf6858d524bdd503e721ba
nattapon.rakthong <nattapon.rakthong@gmail.com>
2020-02-26 20:31:48 +0700

    Config url

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'IFRA IIoT', // Title for your website.
   tagline: 'A website for testing',
   url: 'https://ifraiot.github.io/docs/website/build/test-site/', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: 'https://ifraiot.github.io/docs/website/build/test-site/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
